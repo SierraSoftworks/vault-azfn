bdc2a33293aca2bfe6b980142cddd8cda7727975
Benjamin Pannell <benjamin@pannell.dev>
2022-03-28 21:38:06 +0100

    chore: Bump version of Vault to v1.10.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,26 @@
 variable "location" {
-    description = "The Azure location where this app will be deployed."
+  description = "The Azure location where this app will be deployed."
 }
 
 variable "prefix" {
-    description = "The prefix used to generate globally unique names for resources."
+  description = "The prefix used to generate globally unique names for resources."
 }
 
 variable "suffix" {
-    description = "The suffix used to generate globally unique names for resources."
-    default = ""
+  description = "The suffix used to generate globally unique names for resources."
+  default     = ""
 }
 
 variable "vault_version" {
-    description = "The version of Hashicorp Vault to use."
-    default = "1.9.0"
+  description = "The version of Hashicorp Vault to use."
+  default     = "1.10.0"
 }
 
 variable "vault_agent_version" {
-    description = "The version of the Vault agent to use."
-    default = "1.0.0"
+  description = "The version of the Vault agent to use."
+  default     = "1.2.1"
 }
 
 variable "log_workspace_id" {
-    description = "The workspace ID of the log analytics workspace to use."
+  description = "The workspace ID of the log analytics workspace to use."
 }
