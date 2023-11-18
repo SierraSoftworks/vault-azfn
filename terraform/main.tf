65559e7e108ac62054832e9a8e1f4085a162103d
Benjamin Pannell <benjamin@pannell.dev>
2023-11-18 12:21:39 +0000

    tweak: Update to Vault 1.15.2

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ variable "suffix" {
 
 variable "vault_version" {
   description = "The version of Hashicorp Vault to use."
-  default     = "1.15.1"
+  default     = "1.15.2"
 }
 
 variable "vault_agent_version" {
