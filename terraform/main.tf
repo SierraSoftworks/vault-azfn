16384216a9825b1731d1e165ddd3892b0d320e41
Benjamin Pannell <1760260+notheotherben@users.noreply.github.com>
2024-04-11 01:14:19 +0100

    chore: Update Vault to v1.16.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ variable "suffix" {
 
 variable "vault_version" {
   description = "The version of Hashicorp Vault to use."
-  default     = "1.16.0"
+  default     = "1.16.1"
 }
 
 variable "vault_agent_version" {
