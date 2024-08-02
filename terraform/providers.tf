56cce26f55ffed3a7ad8f149d7ca7fce0ae6617f
Benjamin Pannell <benjamin@pannell.dev>
2024-08-02 11:16:36 +0100

    chore: Switch to OIDC federated auth for terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -30,15 +30,11 @@ provider "azurerm" {
   // NOTE: You can retrieve this secret using `op read op://epfkgzb2bz4msye2xrhffiz3se/jrlwg64m56hkbkbfvgljfkwcfy/Azure/client_secret`
   subscription_id = var.azure_subscription
   tenant_id       = var.azure_tenant
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
 }
 
 provider "azuread" {
   // NOTE: You can retrieve this secret using `op read op://epfkgzb2bz4msye2xrhffiz3se/jrlwg64m56hkbkbfvgljfkwcfy/Azure/client_secret`
-  tenant_id     = var.azure_tenant
-  client_id     = var.azure_client_id
-  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant
 }
 
 variable "azure_subscription" {
@@ -47,12 +43,4 @@ variable "azure_subscription" {
 
 variable "azure_tenant" {
 
-}
-
-variable "azure_client_id" {
-  sensitive = true
-}
-
-variable "azure_client_secret" {
-  sensitive = true
 }
