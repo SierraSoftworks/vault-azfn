0f11701226f0867c8e462f95ff995fb47a8717be
Benjamin Pannell <1760260+notheotherben@users.noreply.github.com>
2026-05-26 18:25:13 +0100

    Update default vault_version to 2.0.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ variable "suffix" {
 
 variable "vault_version" {
   description = "The version of Hashicorp Vault to use."
-  default     = "2.0.0"
+  default     = "2.0.1"
 }
 
 variable "vault_agent_version" {
